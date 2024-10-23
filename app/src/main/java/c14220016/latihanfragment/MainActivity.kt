ad6222f7d32d3950dd89b22ae25f5c9788d1482a
FarrellYunanto <farrellyunanto@gmail.com>
2024-10-23 20:04:45 +0700

    navbar done

--- app/src/main/java/c14220016/latihanfragment/MainActivity.kt
+++ app/src/main/java/c14220016/latihanfragment/MainActivity.kt
@@ -1,6 +1,7 @@
 package c14220016.latihanfragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,47 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val mFragmentManager = supportFragmentManager
+        val mfGame = fgame();
+
+        mFragmentManager.findFragmentByTag(fgame::class.java.simpleName)
+        mFragmentManager
+            .beginTransaction()
+            .add(R.id.mainframe, mfGame, fgame::class.java.simpleName)
+            .commit()
+
+//        Navigation Logic
+        val _buttonToGame = findViewById<Button>(R.id.toGameButton)
+        val _buttonToScore = findViewById<Button>(R.id.toScoreButton)
+        val _buttonToConfig = findViewById<Button>(R.id.toConfigButton)
+
+        _buttonToGame.setOnClickListener {
+            val mfGame = fgame()
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.mainframe, mfGame, fgame::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        _buttonToScore.setOnClickListener {
+            val mfScore = fscore()
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.mainframe, mfScore, fscore::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        _buttonToConfig.setOnClickListener {
+            val mfConfig = fconfig()
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.mainframe, mfConfig, fconfig::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
